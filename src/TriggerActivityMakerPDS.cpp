73baefa987b60db33a87cb30859b43e6da8c51fc
Charlie Batchelor <cbatchel@dunegpvm14.fnal.gov>
2023-04-12 11:24:14 -0500

    New PDS TA and TCMakers

---
+++ src/TriggerActivityMakerPDS.cpp
@@ -0,0 +1,64 @@
+/**
+ * @file TriggerActivityMakerPDS.cpp
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#include "triggeralgs/PDS/TriggerActivityMakerPDS.hpp"
+
+#include "TRACE/trace.h"
+#define TRACE_NAME "TriggerActivityMakerPDS"
+
+#include <vector>
+
+using namespace triggeralgs;
+
+void
+TriggerActivityMakerPDS::operator()(const TriggerPrimitive& input_tp, std::vector<TriggerActivity>& output_ta)
+{
+  if ((m_primitive_count++) % m_prescale == 0)
+  {
+
+    TLOG() << "Emitting PDS Trigger Activity " << (m_primitive_count-1);
+    std::vector<TriggerPrimitive> tp_list;
+    tp_list.push_back(input_tp);
+
+    TriggerActivity ta;
+    ta.time_start = input_tp.time_start;
+    ta.time_end = input_tp.time_start + input_tp.time_over_threshold;
+    ta.time_peak = input_tp.time_peak;
+    ta.time_activity = 0;
+    ta.channel_start = input_tp.channel;
+    ta.channel_end = input_tp.channel;
+    ta.channel_peak = input_tp.channel;
+    ta.adc_integral = input_tp.adc_integral;
+    ta.adc_peak = input_tp.adc_peak;
+    ta.detid = input_tp.detid;
+    ta.type = TriggerActivity::Type::kPDS;
+    ta.algorithm = TriggerActivity::Algorithm::kPDS;
+
+    ta.inputs = tp_list;
+
+    using namespace std::chrono;
+
+    // Update OpMon Variable(s)
+    uint64_t system_time = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+    uint64_t data_time = ta.time_start*16e-6;             // Convert 62.5 MHz ticks to ms    
+    m_data_vs_system_time.store(data_time - system_time); // Store the difference for OpMon
+
+    output_ta.push_back(ta);
+  }
+}
+
+void
+TriggerActivityMakerPDS::configure(const nlohmann::json &config)
+{
+  //FIXME use some schema here
+  if (config.is_object() && config.contains("prescale"))
+  {
+    m_prescale = config["prescale"];
+  }
+  TLOG_DEBUG(TRACE_NAME) << "Using activity prescale " << m_prescale;
+}
---
+++ src/TriggerCandidateMakerPDS.cpp
@@ -0,0 +1,62 @@
+/**
+ * @file TriggerCandidateMakerPDS.cpp
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#include "triggeralgs/PDS/TriggerCandidateMakerPDS.hpp"
+
+#include "TRACE/trace.h"
+#define TRACE_NAME "TriggerCandidateMakerPDS"
+
+#include <vector>
+
+using namespace triggeralgs;
+
+void
+TriggerCandidateMakerPDS::operator()(const TriggerActivity& activity, std::vector<TriggerCandidate>& cand)
+{ 
+  if ((m_activity_count++) % m_prescale == 0)
+  {
+    TLOG(TLVL_DEBUG_1) << "Emitting PDS TriggerCandidate " << (m_activity_count-1);
+
+    std::vector<TriggerActivity::TriggerActivityData> ta_list;
+    ta_list.push_back(static_cast<TriggerActivity::TriggerActivityData>(activity));
+    
+    TriggerCandidate tc;
+    tc.time_start = activity.time_start - m_readout_window_ticks_before;
+    tc.time_end = activity.time_end + m_readout_window_ticks_after;
+    tc.time_candidate = activity.time_start;
+    tc.detid = activity.detid;
+    tc.type = TriggerCandidate::Type::kPDS;
+    tc.algorithm = TriggerCandidate::Algorithm::kPDS;
+
+    tc.inputs = ta_list;
+
+    using namespace std::chrono;
+
+    // Update OpMon Variable(s)
+    uint64_t system_time = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+    uint64_t data_time = activity.time_start*16e-6;       // Convert 62.5 MHz ticks to ms    
+    m_data_vs_system_time.store(data_time - system_time); // Store the difference for OpMon
+
+    cand.push_back(tc);
+  }
+}
+
+void
+TriggerCandidateMakerPDS::configure(const nlohmann::json &config)
+{
+  if (config.is_object() && config.contains("prescale"))
+  {
+    m_prescale = config["prescale"];
+    if (config.contains("readout_window_ticks_before"))
+      m_readout_window_ticks_before = config["readout_window_ticks_before"];
+    if (config.contains("readout_window_ticks_after"))
+      m_readout_window_ticks_after = config["readout_window_ticks_after"];
+
+  }
+  TLOG_DEBUG(TRACE_NAME) << "Using candidate prescale " << m_prescale;
+}
