bddeb806a1964dc5f428a8c090bb41c99386fade
Andrew Mogan <amogan@fnal.gov>
2025-03-24 10:52:32 -0500

    Remove deprecated methods and instantiate local Triton client

--- include/triggeralgs/Triton/TriggerActivityMakerTriton.hpp
+++ include/triggeralgs/Triton/TriggerActivityMakerTriton.hpp
@@ -42,38 +42,29 @@ class TriggerActivityMakerTriton : public TriggerActivityMaker
     //void fail_if_error(const tc::Error& err, const std::string& msg) const;
     //bool warn_if_error(const tc::Error& err, const std::string& msg) const;
     void dump_config() const;
-    void check_triton_server_liveness(const std::string& inference_url) const;
-    //void check_model_readiness(const std::string model_name, const std::string model_version) const;
-    void check_model_readiness(const std::string model_name, const std::string model_version) const;
-    void check_model_inputs(const std::string model_name, const std::string model_version) const;
     std::vector<std::vector<std::vector<int>>> get_adcs_from_trigger_primitives(
       const uint64_t number_tps, 
       const uint64_t time_ticks, 
       const uint64_t number_wires);
-    void ValidateSimpleShapeAndDatatype(const std::string& name, std::shared_ptr<tc::InferResult> result) const;
-    void ValidateSimpleResult(
-      const std::shared_ptr<tc::InferResult> result, 
-      std::vector<int32_t>& input0_data, 
-      std::vector<int32_t>& input1_data) const;
-    //void query_triton_server(const TriggerActivity& trigger_activity, const std::string& inference_url);
     template <typename T> std::string print_collection(triton_span::Span<const T> coll, const std::string& delim);
 
   private:
     //std::unique_ptr<triton::client::InferenceServerGrpcClient> client;
-    std::unique_ptr<triggeralgs::TritonClient> client;
+    std::unique_ptr<triggeralgs::TritonClient> triton_client;
     uint64_t m_number_tps_per_request = 100;
-    uint64_t m_batch_size = 1;
     uint64_t m_number_time_ticks = 128;
     uint64_t m_number_wires = 128;
-    std::string m_inference_url = "localhost:8001";
-    std::string m_model_name = "simple";
-    // The model version is a number representing a directory, so it's declared as a string here
-    std::string m_model_version = "1";
-    uint64_t m_client_timeout_microseconds = 5000;
-    uint64_t m_server_timeout_microseconds = 5000;
-    unsigned m_triton_allowed_tries = 0;
+    uint64_t m_batch_size = 1;
+    std::vector<std::string> m_outputs;
     bool m_print_tp_info = false;
     TriggerActivity m_current_ta;
+    /* Triton config params should now be taken care of in TritonClient class*/
+    //std::string m_inference_url = "localhost:8001";
+    //std::string m_model_name = "simple";
+    // The model version is a number representing a directory, so it's declared as a string here
+    //std::string m_model_version = "1";
+    //uint64_t m_timeout_microseconds = 5000;
+    //unsigned m_triton_allowed_tries = 0;
 };
 
 } // namespace triggeralgs
